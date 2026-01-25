44aabb4073b4aab6b29acc63a4d8c21623eacb74
boss14420 <firefoxlinux@gmail.com>
2026-01-26 02:46:31 +0700

    Do not try to check for cargo by default

--- run_onchange_setup-nvim.sh
+++ run_onchange_setup-nvim.sh
@@ -6,29 +6,31 @@ BOB_INSTALL_DIR="$HOME/.local/share/bob"
 CARGO_DIR="$HOME/.cargo/bin"
 NEOVIM_VERSION="0.10.0"
 BOB_REPO="https://github.com/MordechaiHadad/bob.git"
+BOB_EXECUTABLE=$CARGO_DIR/bob
 
 echo "Ensuring ~/.local/share directory exists..."
 mkdir -p "$HOME/.local/share"
 
-# Check if Rust and Cargo are installed
-if ! command -v cargo &> /dev/null
-then
-    echo "Cargo is not installed. Please install Rust and Cargo (e.g., via rustup.rs) to build bob."
-    exit 1
-fi
-
 # 1. Install bob using cargo build
-if [ ! -f "$CARGO_DIR/bob" ]; then
+if command -v bob &> /dev/null; then
+    echo "bob is already installed."
+    BOB_EXECUTABLE=$(command -v bob)
+else
+    # Check if Rust and Cargo are installed
+    if ! command -v cargo &> /dev/null
+    then
+        echo "Cargo is not installed. Please install Rust and Cargo (e.g., via rustup.rs) to build bob."
+        exit 1
+    fi
+
     echo "Building bob with cargo..."
     cargo install --git $BOB_REPO
     echo "bob installed using cargo build."
-else
-    echo "bob is already installed."
 fi
 
 # 2. Use bob to install nvim 0.10.0
 echo "Installing Neovim $NEOVIM_VERSION using bob..."
-"$CARGO_DIR/bob" use "$NEOVIM_VERSION"
+$BOB_EXECUTABLE use "$NEOVIM_VERSION"
 
 # 3. Install homebrew in current user's home if not installed
 #
